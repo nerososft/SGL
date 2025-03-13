44eaf0c8d9d57324f7d968f4ec343ebb10ec3458
neo <nerosoft@outlook.com>
2025-03-13 20:24:01 +0800

    na: reformat

--- demo/main.cpp
+++ demo/main.cpp
@@ -45,29 +45,29 @@ int main(int argc, char *argv[]) {
     // outputImageInfo.data = malloc(outputImageInfo.width * outputImageInfo.height * outputImageInfo.channels);
     // effectEngine.Process(inputImageInfo, outputImageInfo, filter);
 
-    const auto filter = std::make_shared<GrayFilter>();
-     filter->SetRedFactor(0.299);
-     filter->SetGreenFactor(0.587);
-     filter->SetBlueFactor(0.114);
-     effectEngine.Process("../../demo/images/girl.png", "../../demo/images/girl_gray.png", filter);
+    // const auto filter = std::make_shared<GrayFilter>();
+    // filter->SetRedFactor(0.299);
+    // filter->SetGreenFactor(0.587);
+    // filter->SetBlueFactor(0.114);
+    // effectEngine.Process("../../demo/images/girl.png", "../../demo/images/girl_gray.png", filter);
 
-    const auto filter2 = std::make_shared<GaussianBlurFilter>();
-    filter2->SetRadius(300);
-   // effectEngine.Process("../../demo/images/girl.png", "../../demo/images/girl_blur.png", filter);
-    //const auto filter = std::make_shared<ChinesePaintFilter>();
-    //effectEngine.Process("D:/effect_engine/demo/images/girl.png", "D:/effect_engine/demo/images/test1.png", filter2);
-    effectEngine.Process("D:/effect_engine/demo/images/144-30.png", "D:/effect_engine/demo/images/144-30.png", filter2);
-    //effectEngine.Process("D:/effect_engine/demo/images/girl.png", "D:/effect_engine/demo/images/test.png", filter);
-    //effectEngine.Process("D:/effect_engine/demo/images/girl.png", "D:/effect_engine/demo/images/test.png", filter);
-    //effectEngine.Process("D:/effect_engine/demo/images/girl.png", "D:/effect_engine/demo/images/test.png", filter);
-    //effectEngine.Process("D:/effect_engine/demo/images/girl.png", "D:/effect_engine/demo/images/test.png", filter);
-    //effectEngine.Process("D:/effect_engine/demo/images/girl.png", "D:/effect_engine/demo/images/test.png", filter);
-    //effectEngine.Process("D:/effect_engine/demo/images/girl.png", "D:/effect_engine/demo/images/test.png", filter);
-    //effectEngine.Process("D:/effect_engine/demo/images/girl.png", "D:/effect_engine/demo/images/test.png", filter);
+    // const auto filter2 = std::make_shared<GaussianBlurFilter>();
+    // filter2->SetRadius(300);
+    // effectEngine.Process("../../demo/images/girl.png", "../../demo/images/girl_blur.png", filter);
+    // const auto filter = std::make_shared<ChinesePaintFilter>();
+    // effectEngine.Process("D:/effect_engine/demo/images/girl.png", "D:/effect_engine/demo/images/test1.png", filter2);
+    // effectEngine.Process("D:/effect_engine/demo/images/144-30.png", "D:/effect_engine/demo/images/144-30.png", filter2);
+    // effectEngine.Process("D:/effect_engine/demo/images/girl.png", "D:/effect_engine/demo/images/test.png", filter);
+    // effectEngine.Process("D:/effect_engine/demo/images/girl.png", "D:/effect_engine/demo/images/test.png", filter);
+    // effectEngine.Process("D:/effect_engine/demo/images/girl.png", "D:/effect_engine/demo/images/test.png", filter);
+    // effectEngine.Process("D:/effect_engine/demo/images/girl.png", "D:/effect_engine/demo/images/test.png", filter);
+    // effectEngine.Process("D:/effect_engine/demo/images/girl.png", "D:/effect_engine/demo/images/test.png", filter);
+    // effectEngine.Process("D:/effect_engine/demo/images/girl.png", "D:/effect_engine/demo/images/test.png", filter);
+    // effectEngine.Process("D:/effect_engine/demo/images/girl.png", "D:/effect_engine/demo/images/test.png", filter);
 
-    // // const auto filter = std::make_shared<GaussianBlurFilter>();
-    // // filter->SetRadius(300);
-    // // effectEngine.Process("../../demo/images/girl.png", "../../demo/images/girl_blur.png", filter);
+    // const auto filter = std::make_shared<GaussianBlurFilter>();
+    // filter->SetRadius(300);
+    // effectEngine.Process("../../demo/images/girl.png", "../../demo/images/girl_blur.png", filter);
 
     //const auto filter = std::make_shared<ChinesePaintFilter>();
     //effectEngine.Process("../../demo/images/girl.png", "../../demo/images/girl_chinese_paint.png", filter);
@@ -104,7 +104,7 @@ int main(int argc, char *argv[]) {
     // filter->SetBlurRadius(100);
     // effectEngine.Process("../../demo/images/colorful.png", "../../demo/images/colorful_average_blur.png", filter);
 
-    auto filter = std::make_shared<SurfaceBlurFilter>();
+    const auto filter = std::make_shared<SurfaceBlurFilter>();
     filter->SetBlurRadius(2); // 模糊半径
     filter->SetThreshold(2); // 阈值色阶（2-255）
     effectEngine.Process("../../demo/images/colorful.png", "../../demo/images/colorful_surface_blur.png", filter);
