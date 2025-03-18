3ec25924581d43782924e1b5d0fbcd3eec3c6633
neo <nerosoft@outlook.com>
2025-03-18 12:33:25 +0800

    na

--- effect_engine/config.h
+++ effect_engine/config.h
@@ -5,10 +5,13 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define VERSION "1.0.5"
+#define VERSION "1.0.6"
 
+#ifdef OS_OPEN_HARMONY
+#define SHADER_PATH "/data/storage/el1/bundle/entry/resources/resfile/shader_compiled/"
+#else
 #define SHADER_PATH "../../shader_compiled/"
-
+#endif
 #define SHADER(path) (SHADER_PATH # path)
 
 #endif //CONFIG_H
