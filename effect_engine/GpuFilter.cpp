eb5e68e4133c7780bea55fc262acb1dcb2dde478
huangwei <1600671800@qq.com>
2025-03-18 10:49:36 +0800

    add file

---
+++ effect_engine/GpuFilter.cpp
@@ -0,0 +1,120 @@
+#include <stdint.h>
+
+
+#include <iostream>
+#include "effect_engine/EffectEngine.h"
+#include "effect_engine/filters/impl/ChinesePaintFilter.h"
+#include "effect_engine/filters/impl/ColorPencilSketchFilter.h"
+#include "effect_engine/filters/impl/ColorSeparationFilter.h"
+#include "effect_engine/filters/impl/GaussianBlurFilter.h"
+//#include "effect_engine/filters/impl/thresholdSplit.h"
+#include "GpuFilter.h"
+#include "effect_engine/filters/impl/GrayFilter.h"
+
+
+#include "himi.h"
+//FunctionPtr g_dbg = nullptr;
+FunctionPtr g_dbg ;
+EffectEngine effectEngine;
+
+
+bool threshold_split_filter_gpu(void* in, void* out, int bright) {
+
+   // const auto filter = std::make_shared<thresholdSplitFilter>();
+   // filter->SetBright(bright);
+
+  //  ImageInfo* input = (ImageInfo*)in;
+  //  ImageInfo* output = (ImageInfo*)out;
+
+    //effectEngine.Process(*input, *output, filter);
+    return true;
+
+}
+
+
+bool gaussian_blur_filter_gpu(void* in, void* out, int r) {
+#if ENABLE_HIMI_HILOG
+  //  OH_LOG_INFO(LOG_APP, "hwtest call guassian 3");
+#endif // ENABLE_HIMI_HILOG
+
+
+    const auto filter = std::make_shared<GaussianBlurFilter>();
+    filter->SetRadius(r);
+
+    ImageInfo* input = (ImageInfo*)in;
+    ImageInfo* output = (ImageInfo*)out;
+
+    effectEngine.Process(*input, *output, filter);
+    return true;
+
+}
+
+bool gray_filter_gpu(void* in, void* out, float r ,float g, float b) {
+#if ENABLE_HIMI_HILOG
+  //  OH_LOG_INFO(LOG_APP, "hwtest call gray_filter_gpu");
+#endif // ENABLE_HIMI_HILOG
+
+
+    const auto filter = std::make_shared<GrayFilter>();
+    filter->SetRedFactor(r);
+    filter->SetGreenFactor(g);
+    filter->SetBlueFactor(b);
+
+    ImageInfo* input = (ImageInfo*)in;
+    ImageInfo* output = (ImageInfo*)out;
+
+    effectEngine.Process(*input, *output, filter);
+    return true;
+
+}
+
+
+bool colorSeparation_filter_gpu(void* in, void* out, int roff, int goff, int boff) {
+#if ENABLE_HIMI_HILOG
+   // OH_LOG_INFO(LOG_APP, "hwtest call ColorSeparationFilter");
+#endif // ENABLE_HIMI_HILOG
+
+    const auto filter = std::make_shared<ColorSeparationFilter>();
+ 
+
+     filter->SetRedOffsetX(60.0f);
+    filter->SetGreenOffsetX(120.0f);
+     filter->SetBlueOffsetX(180.0f);
+    ImageInfo* input = (ImageInfo*)in;
+    ImageInfo* output = (ImageInfo*)out;
+
+    effectEngine.Process(*input, *output, filter);
+    return true;
+
+}
+
+
+bool init_gpu_engine() {
+
+#if ENABLE_HIMI_HILOG
+ //   OH_LOG_INFO(LOG_APP, "hwtest lib init");
+#endif // ENABLE_HIMI_HILOG
+    if(g_dbg){
+      //  g_dbg("hwtest g_gbd");
+
+    }
+    return effectEngine.Init();
+
+}
+
+bool destory_gpu_engine() {
+
+    return true;
+
+}
+
+
+
+
+
+bool set_qdebug_cb(void*  dbg){
+
+    //g_dbg = (FunctionPtr)dbg;
+    return true;
+
+}
---
+++ effect_engine/GpuFilter.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+#include <stdint.h>
+
+
+#include <iostream>
+
+
+#define GPU_API __declspec(dllexport)
+
+
+typedef void (*FunctionPtr)(char*);
+
+extern "C"  GPU_API  bool threshold_split_filter_gpu(void* in, void* out, int);
+extern "C"  GPU_API  bool gaussian_blur_filter_gpu(void* in, void* out, int);
+extern "C"  GPU_API  bool init_gpu_engine();
+extern "C"  GPU_API  bool destory_gpu_engine();
+extern "C"  GPU_API  bool set_qdebug_cb(void*  dbg);
+extern "C"  GPU_API  bool gray_filter_gpu(void* in, void* out, float r ,float g, float b);
+extern "C"  GPU_API  bool colorSeparation_filter_gpu(void* in, void* out, int roff, int goff, int boff);
