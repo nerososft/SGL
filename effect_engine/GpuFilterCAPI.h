20b46539355d47b0df05063f72bbd087a97e98df
neo <nerosoft@outlook.com>
2025-03-19 14:33:48 +0800

    na

--- effect_engine/GpuFilterCAPI.h
+++ effect_engine/GpuFilterCAPI.h
@@ -8,6 +8,6 @@ extern "C"  GPU_API  bool init_gpu_engine();
 extern "C"  GPU_API  bool destroy_gpu_engine();
 extern "C"  GPU_API  bool set_debug_cb(void*  dbg);
 extern "C"  GPU_API  bool gray_filter_gpu(void* in, void* out, float r ,float g, float b);
-extern "C"  GPU_API  bool colorSeparation_filter_gpu(void* in, void* out, int roff, int goff, int boff);
+extern "C"  GPU_API  bool color_separation_filter_gpu(void* in, void* out, int roff, int goff, int boff);
 extern "C"  GPU_API	 bool surface_blur_filter_gpu(void* in, void* out, int r, int th);
 extern "C"  GPU_API	 bool adjust_saturation_gpu(void* in, void* out, int v , int s);
