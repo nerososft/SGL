e4bcc475168263d90fd2f4b5607d179d811a290d
huangwei <1600671800@qq.com>
2025-03-19 09:22:51 +0800

    add adjust saturation test

--- effect_engine/GpuFilter.cpp
+++ effect_engine/GpuFilter.cpp
@@ -11,6 +11,9 @@
 #include "GpuFilter.h"
 #include "effect_engine/filters/impl/GrayFilter.h"
 #include "effect_engine/filters/impl/OldGaussianBlurFilter.h"
+#include "effect_engine/filters/impl/AverageBlurFilter.h"
+#include "effect_engine/filters/impl/SurfaceBlurFilter.h"
+#include "effect_engine/filters/impl/adjustSaturation.h"
 
 #include "himi.h"
 //FunctionPtr g_dbg = nullptr;
@@ -32,6 +35,7 @@ bool threshold_split_filter_gpu(void* in, void* out, int bright) {
 }
 
 
+//高斯模糊
 bool gaussian_blur_filter_gpu(void* in, void* out, int r) {
 #if ENABLE_HIMI_HILOG
   //  OH_LOG_INFO(LOG_APP, "hwtest call guassian 3");
@@ -48,6 +52,49 @@ bool gaussian_blur_filter_gpu(void* in, void* out, int r) {
     return true;
 
 }
+
+//表面模糊
+bool surface_blur_filter_gpu(void* in, void* out, int r, int th) {
+    const auto filter = std::make_shared<SurfaceBlurFilter>();
+    filter->SetBlurRadius(r);
+    filter->SetThreshold(th);
+
+    ImageInfo* input = (ImageInfo*)in;
+    ImageInfo* output = (ImageInfo*)out;
+
+    effectEngine.Process(*input, *output, filter);
+    return true;
+
+}
+
+//平均模糊
+bool average_blur_filter_gpu(void* in, void* out) {
+    const auto filter = std::make_shared<AverageBlurFilter>();
+
+    ImageInfo* input = (ImageInfo*)in;
+    ImageInfo* output = (ImageInfo*)out;
+
+    effectEngine.Process(*input, *output, filter);
+    return true;
+
+}
+
+bool adjust_saturation_gpu(void* in, void* out , int v, int s)
+{
+    
+    const auto filter = std::make_shared<adjustSaturationFilter>();
+
+    filter->SetVibrance(v);
+    filter->SetSaturation(s);
+
+    ImageInfo* input = (ImageInfo*)in;
+    ImageInfo* output = (ImageInfo*)out;
+
+    effectEngine.Process(*input, *output, filter);
+ 
+    return true;
+}
+
 
 bool gray_filter_gpu(void* in, void* out, float r ,float g, float b) {
 #if ENABLE_HIMI_HILOG
--- effect_engine/GpuFilter.h
+++ effect_engine/GpuFilter.h
@@ -18,4 +18,11 @@ extern "C"  GPU_API  bool init_gpu_engine();
 extern "C"  GPU_API  bool destory_gpu_engine();
 extern "C"  GPU_API  bool set_qdebug_cb(void*  dbg);
 extern "C"  GPU_API  bool gray_filter_gpu(void* in, void* out, float r ,float g, float b);
+//颜色分离 华为建议加上
 extern "C"  GPU_API  bool colorSeparation_filter_gpu(void* in, void* out, int roff, int goff, int boff);
+//表面模糊
+extern "C"  GPU_API	 bool surface_blur_filter_gpu(void* in, void* out, int r, int th);
+//平均模糊
+extern "C"  GPU_API	 bool average_blur_filter_gpu(void* in, void* out);
+//饱和度
+extern "C"  GPU_API	 bool adjust_saturation_gpu(void* in, void* out, int v , int s);
