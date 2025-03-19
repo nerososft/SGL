993712036860b4592d18bf715dad4c0dba6bed91
neo <nerosoft@outlook.com>
2025-03-19 20:48:22 +0800

    na

--- demo/main.cpp
+++ demo/main.cpp
@@ -157,9 +157,9 @@ int main(int argc, char *argv[]) {
     // effectEngine.Process("../../demo/images/girl.png", "../../demo/images/girl_threshold.png", filter);
 
     const auto filter = std::make_shared<VoronoiFilter>();
-    filter->SetGridSize(100);
+    filter->SetGridSize(64);
     filter->SetEnableEdge(true);
-    effectEngine.Process("../../demo/images/girl.png", "../../demo/images/girl_voronoi.png", filter);
+    effectEngine.Process("../../demo/images/colorful.png", "../../demo/images/colorful_voronoi.png", filter);
 
     // getchar();
     return 0;
