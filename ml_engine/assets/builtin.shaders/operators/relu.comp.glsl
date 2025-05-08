b87f3c3b4a613d3b1d77334e3fa991aaa4c871a2
neo <nerosoft@outlook.com>
2025-05-08 14:58:07 +0800

    na

---
+++ ml_engine/assets/builtin.shaders/operators/relu.comp.glsl
@@ -0,0 +1,25 @@
+#version 450
+
+layout (local_size_x = 256, local_size_y = 1, local_size_z = 1) in;
+
+layout (std430, binding = 0) buffer InputStorageBuffer {
+    float data[];
+} inputBuffer;
+
+layout (std430, binding = 1) buffer OutputStorageBuffer {
+    float data[];
+} outputBuffer;
+
+layout (push_constant) uniform Params {
+    uint width;
+    uint height;
+// TODO: params
+} params;
+
+void main() {
+    uvec2 coord = gl_GlobalInvocationID.xy;
+    if (coord.x >= inputBuffer.data.length()) {
+        return;
+    }
+    inputBuffer.data[coord.x] = inputBuffer.data[coord.x] > 0 ? inputBuffer.data[coord.x]:0;
+}
