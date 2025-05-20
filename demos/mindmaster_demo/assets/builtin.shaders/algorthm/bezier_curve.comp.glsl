81bbdd553485ee58a773e3e0356fad64e62fe98b
neo <nerosoft@outlook.com>
2025-05-20 11:22:47 +0800

    bezier curve

--- demos/mindmaster_demo/assets/builtin.shaders/algorthm/bezier_curve.comp.glsl
+++ demos/mindmaster_demo/assets/builtin.shaders/algorthm/bezier_curve.comp.glsl
@@ -1,6 +1,6 @@
 #version 450
 
-layout (local_size_x = 32, local_size_y = 32, local_size_z = 1) in;
+layout (local_size_x = 256, local_size_y = 1, local_size_z = 1) in;
 
 layout (std430, binding = 0) buffer PointData {
     vec4 controlPoints[4];
@@ -13,9 +13,27 @@ layout (std430, binding = 1) buffer OutputData {
 
 layout (push_constant) uniform Params {
     uint width;
-    // TODO:
+// TODO:
 } params;
+
+vec4 calculateBezierPoint(vec4 P0, vec4 P1, vec4 P2, vec4 P3, float t) {
+    return (1.0 - t) * (1.0 - t) * (1.0 - t) * P0 +
+    3.0 * (1.0 - t) * (1.0 - t) * t * P1 +
+    3.0 * (1.0 - t) * t * t * P2 +
+    t * t * t * P3;
+}
 
 void main() {
     uvec2 coord = gl_GlobalInvocationID.xy;
+    float t = float(coord.x) / float(pointData.numSegments);
+
+    // Calculate the point on the Bezier curve
+    vec4 bezierPoint = calculateBezierPoint(
+    pointData.controlPoints[0],
+    pointData.controlPoints[1],
+    pointData.controlPoints[2],
+    pointData.controlPoints[3],
+    t);
+
+    outputData.points[coord.x] = bezierPoint;
 }
--- demos/mindmaster_demo/main.cpp
+++ demos/mindmaster_demo/main.cpp
@@ -3,9 +3,87 @@
 //
 
 #include <iostream>
+#include <memory>
 #include <ostream>
+#include <vector>
+#include <gpu_engine/config.h>
+#include <gpu_engine/gpu/VkGPUBuffer.h>
+#include <gpu_engine/gpu/VkGPUContext.h>
+#include <gpu_engine/gpu/compute_graph/ComputeGraph.h>
+#include <gpu_engine/gpu/compute_graph/ComputePipelineNode.h>
+
+bool bezier_curve() {
+    std::vector<const char *> extensions = {};
+    auto gpuCtx = std::make_shared<VkGPUContext>(extensions);
+
+    if (gpuCtx->Init() != VK_SUCCESS) {
+        std::cerr << "Failed to initialize GPU context!" << std::endl;
+        return false;
+    }
+
+    const auto computeGraph = std::make_shared<ComputeGraph>(gpuCtx);
+    const auto computeSubGraph = std::make_shared<SubComputeGraph>(gpuCtx);
+    if (computeSubGraph->Init() != VK_SUCCESS) {
+        std::cerr << "Failed to initialize sub graph!" << std::endl;
+        return false;
+    }
+    computeGraph->AddSubGraph(computeSubGraph);
+
+    VkDeviceSize controlDataSize = 1; // TODO:
+    const auto inputBuffer = std::make_shared<VkGPUBuffer>(gpuCtx);
+    inputBuffer->AllocateAndBind(GPU_BUFFER_TYPE_STORAGE_SHARED, controlDataSize);
+
+    VkDeviceSize outputPointDataSize = 1; // TODO:
+    const auto outputBuffer = std::make_shared<VkGPUBuffer>(gpuCtx);
+    outputBuffer->AllocateAndBind(GPU_BUFFER_TYPE_STORAGE_SHARED, outputPointDataSize);
+
+    const auto bezierNode = std::make_shared<ComputePipelineNode>(gpuCtx, "bezier_curve",
+                                                                  SHADER(bezier_curve.comp.glsl.spv),
+                                                                  256,
+                                                                  1,
+                                                                  1);
+
+    std::vector<PipelineNodeBuffer> ppBuffers;
+    ppBuffers.push_back({
+        .type = PIPELINE_NODE_BUFFER_STORAGE_READ,
+        .bufferSize = inputBuffer->GetBufferSize(),
+        .buffer = inputBuffer->GetBuffer()
+    });
+    ppBuffers.push_back({
+        .type = PIPELINE_NODE_BUFFER_STORAGE_WRITE,
+        .bufferSize = outputBuffer->GetBufferSize(),
+        .buffer = outputBuffer->GetBuffer()
+    });
+
+    const PushConstantInfo pushConstantInfo{};
+    const ComputeElement element = {
+        .pushConstantInfo = pushConstantInfo,
+        .buffers = ppBuffers,
+        .customDrawFunc = nullptr
+    };
+
+    bezierNode->AddComputeElement(element);
+
+    if (bezierNode->CreateComputeGraphNode() != VK_SUCCESS) {
+        std::cerr << "Failed to create compute graph node!" << std::endl;
+        return false;
+    }
+
+    computeSubGraph->AddComputeGraphNode(bezierNode);
+
+
+    const VkResult ret = computeGraph->Compute();
+    if (ret != VK_SUCCESS) {
+        std::cerr << "Failed to compute graph!" << std::endl;
+        return false;
+    }
+    return true;
+}
 
 int main(int argc, char *argv[]) {
     std::cout << "mindmaster_demo" << std::endl;
+
+    bezier_curve();
+
     return 0;
 }
