f1bb71e467a0086b0a5224adaa4b075662251182
neo <nerosoft@outlook.com>
2025-06-17 16:49:28 +0800

    na

--- demos/2d_renderer_demo/main.cpp
+++ demos/2d_renderer_demo/main.cpp
@@ -1,3 +1,4 @@
+#include "Painter.h"
 #include "core/log/Log.h"
 //
 // Created by neo on 25-6-17.
@@ -5,5 +6,10 @@
 int main(int argc, char *argv[]) {
     Logger() << "Hello World!\n";
     // TODO: 2d painter impl for qt
+
+    Painter painter;
+    painter.Init(1024,768);
+
+    painter.toImage("../../../demos/2d_renderer_demo/painter.png");
     return 0;
 }
