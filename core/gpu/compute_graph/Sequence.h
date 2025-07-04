983d5e511591e19e558d202d6575012e61994148
neo <nerosoft@outlook.com>
2025-07-04 10:12:21 +0800

    na

---
+++ core/gpu/compute_graph/Sequence.h
@@ -0,0 +1,29 @@
+//
+// Created by neo on 25-7-3.
+//
+
+#ifndef SEQUENCE_H
+#define SEQUENCE_H
+#include "core/gpu/compute_graph/ComputeGraph.h"
+
+class Sequence : public std::enable_shared_from_this<Sequence> {
+    std::shared_ptr<VkGPUContext> gpuCtx = nullptr;
+    std::shared_ptr<ComputeGraph> graph = nullptr;
+    std::shared_ptr<SubComputeGraph> mainSubGraph = nullptr;
+
+public:
+    explicit Sequence(const std::shared_ptr<VkGPUContext> &gpuCtx);
+
+    bool Init();
+
+    ~Sequence() = default;
+
+    std::shared_ptr<Sequence> Record(const std::shared_ptr<IComputeGraphNode> &node);
+
+    std::shared_ptr<Sequence> Eval();
+
+    std::shared_ptr<Sequence> Destroy();
+};
+
+
+#endif //SEQUENCE_H
