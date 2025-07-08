7e97c24bbd81158e2a31d1d3b9647d7b2765e2f8
neo <nerosoft@outlook.com>
2025-07-08 17:59:34 +0800

    na

--- engine/effect/filters/impl/YuvToRgbFilter.cpp
+++ engine/effect/filters/impl/YuvToRgbFilter.cpp
@@ -20,8 +20,8 @@ VkResult YuvToRgbFilter::Apply(const std::shared_ptr<VkGPUContext> &gpuCtx,
                                const std::vector<FilterImageInfo> &inputImageInfo,
                                const std::vector<FilterImageInfo> &outputImageInfo) {
     BasicFilterParams params;
-    this->yuvToRgbFilterParams.imageSize.width = inputImageInfo[0].width;
-    this->yuvToRgbFilterParams.imageSize.height = inputImageInfo[0].height;
+    this->yuvToRgbFilterParams.imageSize.width = outputImageInfo[0].width;
+    this->yuvToRgbFilterParams.imageSize.height = outputImageInfo[0].height;
     this->yuvToRgbFilterParams.imageSize.channels = 4;
     this->yuvToRgbFilterParams.imageSize.bytesPerLine =
             this->yuvToRgbFilterParams.imageSize.width * 4;
