52340f92a368d19d5b9ba57b0fcf13768c1d5aa6
neoyang <neoyang@noreply.gitcode.com>
2025-07-17 16:58:15 +0800

    na

--- examples/inference_demo/models/Qwen3_0_6B/qwen3_0_6b.py
+++ examples/inference_demo/models/Qwen3_0_6B/qwen3_0_6b.py
@@ -1,13 +1,45 @@
 from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 
 # 加载本地模型（确保模型文件在当前目录或指定路径）
-model_path = "./"  # 替换为实际模型路径
+model_path = "../Qwen3-0.6B"  # 替换为实际模型路径
 
-# 方法2: 手动加载模型和分词器
+# load the tokenizer and the model
 tokenizer = AutoTokenizer.from_pretrained(model_path)
-model = AutoModelForCausalLM.from_pretrained(model_path)
+model = AutoModelForCausalLM.from_pretrained(
+    model_path,
+    torch_dtype="auto",
+    device_map="auto"
+)
 
-inputs = tokenizer("What is the capital of France?", return_tensors="pt")
-outputs = model.generate(**inputs, max_length=256)
-generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-print("Manual Model Output:", generated_text)
+# prepare the model input
+prompt = "Give me a short introduction to large language model."
+messages = [
+    {"role": "user", "content": prompt}
+]
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True,
+    enable_thinking=True # Switches between thinking and non-thinking modes. Default is True.
+)
+model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+# conduct text completion
+generated_ids = model.generate(
+    **model_inputs,
+    max_new_tokens=32768
+)
+output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist()
+
+# parsing thinking content
+try:
+    # rindex finding 151668 (</think>)
+    index = len(output_ids) - output_ids[::-1].index(151668)
+except ValueError:
+    index = 0
+
+thinking_content = tokenizer.decode(output_ids[:index], skip_special_tokens=True).strip("\n")
+content = tokenizer.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
+
+print("thinking content:", thinking_content)
+print("content:", content)
