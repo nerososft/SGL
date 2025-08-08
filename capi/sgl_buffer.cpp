f4d1326db11485a2012bb9bb2ae48326588459bc
neoyang <neoyang@noreply.gitcode.com>
2025-08-08 17:56:31 +0800

    na

--- capi/sgl_buffer.cpp
+++ capi/sgl_buffer.cpp
@@ -22,12 +22,14 @@ sgl_buffer_t sgl_buffer_manager_allocate_buffer(const sgl_buffer_manager *mgr,
   VkResult ret =
       buffer->AllocateAndBind(static_cast<VkGPUBufferType>(type), size);
   if (ret != VK_SUCCESS) {
+    Logger() << Logger::ERROR << "Failed to allocate gpu buffer!" << std::endl;
     sgl_buffer_t buf{};
     buf.type = SGL_BUFFER_TYPE_UNKNOWN;
     return buf;
   }
   ret = buffer->MapBuffer(size);
   if (ret != VK_SUCCESS) {
+    Logger() << Logger::ERROR << "Failed to map gpu buffer!" << std::endl;
     buffer->Destroy();
     sgl_buffer_t buf{};
     buf.type = SGL_BUFFER_TYPE_UNKNOWN;
@@ -50,12 +52,14 @@ sgl_error_t sgl_buffer_manager_destroy_buffer(const sgl_buffer_manager *mgr,
 
 sgl_buffer_manager_t *sgl_buffer_manager_create(const sgl_gpu_ctx_t *gpu_ctx) {
   if (gpu_ctx == nullptr) {
-    Logger() << "gpu_ctx is null." << std::endl;
+    Logger() << Logger::ERROR << "gpu_ctx is null." << std::endl;
     return nullptr;
   }
   const auto bufMgr =
       static_cast<sgl_buffer_manager_t *>(malloc(sizeof(sgl_buffer_manager_t)));
   if (bufMgr == nullptr) {
+    Logger() << Logger::ERROR << "Failed to allocate sgl_buffer_manager_t."
+             << std::endl;
     return nullptr;
   }
 
