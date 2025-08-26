333e98e0cb5c3ab7a1803d7f323aa86f03c779b9
neoyang <neoyang@noreply.gitcode.com>
2025-08-26 17:28:27 +0800

    na

--- capi/include/sgl/sgl_compute.h
+++ capi/include/sgl/sgl_compute.h
@@ -4,6 +4,7 @@
 
 #ifndef SGL_COMPUTE_H
 #define SGL_COMPUTE_H
+#include "sgl_buffer.h"
 #include "sgl_error.h"
 #include "sgl_gpu_ctx.h"
 #include <stddef.h>
@@ -11,11 +12,15 @@
 typedef struct sgl_tensor {
   size_t ndim;
   size_t *shape;
-  float *data;
+  sgl_buffer_t gpuBuf;
 } sgl_tensor_t;
 
 typedef struct sgl_compute {
-  sgl_error_t (*mat_mul)(sgl_tensor_t mat1, sgl_tensor_t mat2,
+  sgl_tensor_t *(*tensor_create)(sgl_buffer_manager_t *buf_mgr, size_t ndim,
+                                 size_t *shape, void *data);
+  sgl_error_t (*tensor_destroy)(sgl_buffer_manager_t *buf_mgr,
+                                sgl_tensor_t *tensor);
+  sgl_error_t (*mat_mul)(sgl_tensor_t *mat1, sgl_tensor_t *mat2,
                          sgl_tensor_t *out);
   sgl_error_t (*record)();
   sgl_error_t (*eval)(struct sgl_compute *cc);
--- capi/sgl_compute.cpp
+++ capi/sgl_compute.cpp
@@ -5,6 +5,69 @@
 #include "include/sgl/sgl_compute.h"
 #include "runtime/log/Log.h"
 
+sgl_tensor_t *sgl_tensor_create(const sgl_buffer_manager_t *buf_mgr,
+                                const size_t ndim, size_t *shape,
+                                const void *data) {
+  if (buf_mgr == nullptr) {
+    Logger() << "buffer manager is null" << std::endl;
+    return nullptr;
+  }
+  if (shape == nullptr) {
+    Logger() << "shape is nullptr" << std::endl;
+    return nullptr;
+  }
+  const auto tensor = static_cast<sgl_tensor_t *>(malloc(sizeof(sgl_tensor_t)));
+  if (tensor == nullptr) {
+    Logger() << "malloc failed" << std::endl;
+    return nullptr;
+  }
+  tensor->ndim = ndim;
+  tensor->shape = shape;
+  size_t totalSize = 0;
+  for (size_t i = 0; i < ndim; ++i) {
+    totalSize += shape[i];
+  }
+  if (totalSize <= 0) {
+    Logger() << "totalSize is 0" << std::endl;
+    free(tensor);
+    return nullptr;
+  }
+
+  const auto buf = buf_mgr->allocate_buffer(
+      buf_mgr, SGL_BUFFER_TYPE_STORAGE_SHARED, totalSize);
+  if (buf.type == SGL_BUFFER_TYPE_UNKNOWN) {
+    Logger() << "gpu mem allocated failed" << std::endl;
+    free(tensor);
+    return nullptr;
+  }
+  tensor->gpuBuf = buf;
+
+  if (data != nullptr) {
+    memcpy(buf.data, data, totalSize);
+  }
+  return tensor;
+}
+
+sgl_error_t sgl_tensor_destroy(const sgl_buffer_manager_t *buf_mgr,
+                               sgl_tensor_t *tensor) {
+  if (tensor == nullptr) {
+    Logger() << "tensor is nullptr" << std::endl;
+    return SGL_INVALID_ARGUMENT;
+  }
+  if (buf_mgr == nullptr) {
+    Logger() << "buf manager is null" << std::endl;
+    return SGL_INVALID_ARGUMENT;
+  }
+
+  const sgl_error_t err = buf_mgr->destroy_buffer(buf_mgr, &tensor->gpuBuf);
+  if (err != SGL_SUCCESS) {
+    Logger() << "destroy buffer failed" << std::endl;
+    return err;
+  }
+  free(tensor);
+  return SGL_SUCCESS;
+}
+
 sgl_compute_t *sgl_compute_create(const sgl_gpu_ctx_t *gpu_ctx) {
   if (gpu_ctx == nullptr) {
     Logger() << "gpu_ctx is null." << std::endl;
